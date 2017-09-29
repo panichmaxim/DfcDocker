07759a510484909753dc897db956f6cf7a804f6b
Panich Maxim <panichmax@gmail.com>
2017-09-29 06:46:51 +0300

    Post file

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import jsonify
+from flask import request
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,3 +10,11 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
+    
+@app.route('/estimate', methods=['POST'])
+def upload():
+    try:
+        imagefile = flask.request.files.get('imagefile', '')
+        return jsonify({'estimate': imagefile})
+    except Exception as err:
+        return jsonify({'error': "Error loading file"})
