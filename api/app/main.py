fe66c84802ddc44e8da475cc8fc9a47a37ca1a0e
Panich Maxim <panichmax@gmail.com>
2017-09-29 06:54:05 +0300

    Improve

--- api/app/main.py
+++ api/app/main.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World from Flask"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
     
 @app.route('/estimate', methods=['POST'])
 def upload():
     try:
-        imagefile = flask.request.files.get('imagefile', '')
+        imagefile = flask.request.files.get('image', '')
         return jsonify({'estimate': imagefile})
     except Exception as err:
         return jsonify({'error': "Error loading file"})
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=80)
