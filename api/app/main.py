3fb3191e00287de22c2f0308e032f368d2dfc4c1
Panich Maxim <panichmax@gmail.com>
2017-10-01 14:14:26 +0300

    Update

--- api/app/main.py
+++ api/app/main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Cool landing"
-    
+
 @app.route('/estimate', methods=['POST'])
 def estimate():
     try:
@@ -19,6 +19,11 @@ def estimate():
         return jsonify({'filename': f_name})
     except Exception as err:
         return jsonify({'error': str(err)})
-        
+
+@app.route('/price', methods=['GET'])
+def estimate():
+    address = request.args.get("address")
+    return jsonify({'flat_price': "6854710"})
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=80)
